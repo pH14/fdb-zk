1235ad6e66e67b80accd9be5e4a828a8ef5f509f
Paul Hemberger <phemberger@hubspot.com>
2019-03-04 23:18:52 -0500

    delete op

---
+++ src/main/java/com/ph14/fdb/zk/ops/FdbDeleteOp.java
@@ -0,0 +1,89 @@
+package com.ph14.fdb.zk.ops;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException.BadVersionException;
+import org.apache.zookeeper.KeeperException.NoNodeException;
+import org.apache.zookeeper.KeeperException.NotEmptyException;
+import org.apache.zookeeper.OpResult.DeleteResult;
+import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.proto.DeleteRequest;
+import org.apache.zookeeper.server.Request;
+
+import com.apple.foundationdb.Transaction;
+import com.apple.foundationdb.directory.DirectoryLayer;
+import com.apple.foundationdb.directory.DirectorySubspace;
+import com.apple.foundationdb.directory.NoSuchDirectoryException;
+import com.google.common.collect.ImmutableMap;
+import com.google.inject.Inject;
+import com.hubspot.algebra.Result;
+import com.hubspot.algebra.Results;
+import com.ph14.fdb.zk.layer.FdbNodeReader;
+import com.ph14.fdb.zk.layer.FdbNodeWriter;
+import com.ph14.fdb.zk.layer.FdbPath;
+import com.ph14.fdb.zk.layer.FdbWatchManager;
+import com.ph14.fdb.zk.layer.StatKey;
+
+public class FdbDeleteOp implements FdbOp<DeleteRequest, DeleteResult> {
+
+  private static final int ALL_VERSIONS_FLAG = -1;
+
+  private final FdbNodeReader fdbNodeReader;
+  private final FdbNodeWriter fdbNodeWriter;
+  private final FdbWatchManager fdbWatchManager;
+
+  @Inject
+  public FdbDeleteOp(FdbNodeReader fdbNodeReader,
+                     FdbNodeWriter fdbNodeWriter,
+                     FdbWatchManager fdbWatchManager) {
+    this.fdbNodeReader = fdbNodeReader;
+    this.fdbNodeWriter = fdbNodeWriter;
+    this.fdbWatchManager = fdbWatchManager;
+  }
+
+  @Override
+  public CompletableFuture<Result<DeleteResult, KeeperException>> execute(Request zkRequest, Transaction transaction, DeleteRequest request) {
+    List<String> path = FdbPath.toFdbPath(request.getPath());
+
+    final DirectorySubspace nodeSubspace;
+    final Stat stat;
+    try {
+      nodeSubspace = DirectoryLayer.getDefault().open(transaction, path).join();
+      stat = fdbNodeReader.getNodeStat(nodeSubspace, transaction).join();
+
+      if (stat.getNumChildren() != 0) {
+        return CompletableFuture.completedFuture(Result.err(new NotEmptyException(request.getPath())));
+      }
+
+      if (request.getVersion() != ALL_VERSIONS_FLAG && stat.getVersion() != request.getVersion()) {
+        return CompletableFuture.completedFuture(Result.err(new BadVersionException(request.getPath())));
+      }
+
+      DirectorySubspace parentSubspace = DirectoryLayer.getDefault().open(transaction, FdbPath.toFdbParentPath(request.getPath())).join();
+
+      fdbNodeWriter.writeStat(transaction, parentSubspace,
+          ImmutableMap.of(
+              StatKey.PZXID, FdbNodeWriter.VERSIONSTAMP_FLAG,
+              StatKey.CVERSION, FdbNodeWriter.INCREMENT_INT_FLAG,
+              StatKey.NUM_CHILDREN, FdbNodeWriter.DECREMENT_INT_FLAG
+          ));
+
+      fdbNodeWriter.deleteNode(transaction, nodeSubspace);
+
+      fdbWatchManager.triggerNodeDeletedWatch(transaction, request.getPath());
+      fdbWatchManager.triggerNodeChildrenWatch(transaction, request.getPath());
+
+      return CompletableFuture.completedFuture(Result.ok(new DeleteResult()));
+    } catch (CompletionException e) {
+      if (e.getCause() instanceof NoSuchDirectoryException) {
+        return CompletableFuture.completedFuture(Results.err(new NoNodeException(request.getPath())));
+      } else {
+        throw new RuntimeException(e);
+      }
+    }
+  }
+
+}
---
+++ src/test/java/com/ph14/fdb/zk/ops/FdbDeleteOpTest.java
@@ -0,0 +1,93 @@
+package com.ph14.fdb.zk.ops;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException.Code;
+import org.apache.zookeeper.OpResult.DeleteResult;
+import org.apache.zookeeper.proto.CreateRequest;
+import org.apache.zookeeper.proto.DeleteRequest;
+import org.apache.zookeeper.proto.ExistsRequest;
+import org.apache.zookeeper.proto.ExistsResponse;
+import org.apache.zookeeper.proto.SetDataRequest;
+import org.junit.Test;
+
+import com.hubspot.algebra.Result;
+import com.ph14.fdb.zk.FdbBaseTest;
+
+public class FdbDeleteOpTest extends FdbBaseTest {
+
+  @Test
+  public void itReturnsErrorIfNodeDoesntExist() {
+    Result<DeleteResult, KeeperException> result = fdb.run(
+        tr -> fdbDeleteOp.execute(REQUEST, tr, new DeleteRequest(BASE_PATH, 0))).join();
+    assertThat(result.unwrapErrOrElseThrow().code()).isEqualTo(Code.NONODE);
+  }
+
+  @Test
+  public void itReturnsErrorIfVersionDoesntMatch() {
+    fdb.run(tr -> fdbCreateOp.execute(REQUEST, tr, new CreateRequest(BASE_PATH, new byte[0], Collections.emptyList(), 0))).join();
+    fdb.run(tr -> fdbSetDataOp.execute(REQUEST, tr, new SetDataRequest(BASE_PATH, "a".getBytes(), 1))).join();
+    fdb.run(tr -> fdbSetDataOp.execute(REQUEST, tr, new SetDataRequest(BASE_PATH, "b".getBytes(), 2))).join();
+
+    Result<DeleteResult, KeeperException> result = fdb.run(
+        tr -> fdbDeleteOp.execute(REQUEST, tr, new DeleteRequest(BASE_PATH, 2))).join();
+
+    assertThat(result.unwrapErrOrElseThrow().code()).isEqualTo(Code.BADVERSION);
+  }
+
+  @Test
+  public void itReturnsErrorIfNodeHasChildren() {
+    fdb.run(tr -> fdbCreateOp.execute(REQUEST, tr, new CreateRequest(BASE_PATH, new byte[0], Collections.emptyList(), 0))).join();
+    fdb.run(tr -> fdbCreateOp.execute(REQUEST, tr, new CreateRequest(SUBPATH, new byte[0], Collections.emptyList(), 0))).join();
+
+    Result<DeleteResult, KeeperException> result = fdb.run(
+        tr -> fdbDeleteOp.execute(REQUEST, tr, new DeleteRequest(BASE_PATH, 1))).join();
+
+    assertThat(result.unwrapErrOrElseThrow().code()).isEqualTo(Code.NOTEMPTY);
+  }
+
+  @Test
+  public void itDeletesIfVersionMatchesExactly() {
+    fdb.run(tr -> fdbCreateOp.execute(REQUEST, tr, new CreateRequest(BASE_PATH, new byte[0], Collections.emptyList(), 0))).join();
+    fdb.run(tr -> fdbSetDataOp.execute(REQUEST, tr, new SetDataRequest(BASE_PATH, "a".getBytes(), 0))).join();
+    fdb.run(tr -> fdbSetDataOp.execute(REQUEST, tr, new SetDataRequest(BASE_PATH, "b".getBytes(), 1))).join();
+
+    Result<DeleteResult, KeeperException> result = fdb.run(
+        tr -> fdbDeleteOp.execute(REQUEST, tr, new DeleteRequest(BASE_PATH, 2))).join();
+
+    assertThat(result.unwrapOrElseThrow()).isEqualTo(new DeleteResult());
+  }
+
+  @Test
+  public void itDeletesIfVersionIsAllVersionsFlag() {
+    fdb.run(tr -> fdbCreateOp.execute(REQUEST, tr, new CreateRequest(BASE_PATH, new byte[0], Collections.emptyList(), 0))).join();
+
+    Result<DeleteResult, KeeperException> result = fdb.run(
+        tr -> fdbDeleteOp.execute(REQUEST, tr, new DeleteRequest(BASE_PATH, -1))).join();
+
+    assertThat(result.unwrapOrElseThrow()).isEqualTo(new DeleteResult());
+  }
+
+  @Test
+  public void itUpdatesParentStatAfterSuccessfulDeletion() {
+    fdb.run(tr -> fdbCreateOp.execute(REQUEST, tr, new CreateRequest(BASE_PATH, new byte[0], Collections.emptyList(), 0))).join();
+    fdb.run(tr -> fdbCreateOp.execute(REQUEST, tr, new CreateRequest(SUBPATH, new byte[0], Collections.emptyList(), 0))).join();
+
+    Result<ExistsResponse, KeeperException> exists = fdb.run(
+        tr -> fdbExistsOp.execute(REQUEST, tr, new ExistsRequest(BASE_PATH, false))).join();
+    assertThat(exists.unwrapOrElseThrow().getStat().getCversion()).isEqualTo(1);
+    assertThat(exists.unwrapOrElseThrow().getStat().getNumChildren()).isEqualTo(1);
+    long initialPzxid = exists.unwrapOrElseThrow().getStat().getPzxid();
+
+    fdb.run(tr -> fdbDeleteOp.execute(REQUEST, tr, new DeleteRequest(SUBPATH, 0))).join();
+
+    exists = fdb.run(tr -> fdbExistsOp.execute(REQUEST, tr, new ExistsRequest(BASE_PATH, false))).join();
+    assertThat(exists.unwrapOrElseThrow().getStat().getCversion()).isEqualTo(2);
+    assertThat(exists.unwrapOrElseThrow().getStat().getNumChildren()).isEqualTo(0);
+    assertThat(exists.unwrapOrElseThrow().getStat().getPzxid()).isGreaterThan(initialPzxid);
+  }
+
+}
